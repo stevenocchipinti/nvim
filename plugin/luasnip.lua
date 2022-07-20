58e0a3876024554c5dd7217fd24d207165c772d6
Steve Occhipinti <dev@stevenocchipinti.com>
2022-07-20 23:04:02 +1000

    Add some js/ts/react lua snippets

--- plugin/luasnip.lua
+++ plugin/luasnip.lua
@@ -1,4 +1,19 @@
 local luasnip = require("luasnip")
+local snip = luasnip.snippet
+-- local sn = luasnip.snippet_node
+-- local isn = luasnip.indent_snippet_node
+local t = luasnip.text_node
+local i = luasnip.insert_node
+local f = luasnip.function_node
+-- local c = luasnip.choice_node
+-- local d = luasnip.dynamic_node
+-- local r = luasnip.restore_node
+-- local events = require("luasnip.util.events")
+-- local ai = require("luasnip.nodes.absolute_indexer")
+local fmt = require("luasnip.extras.fmt").fmt
+-- local m = require("luasnip.extras").m
+-- local lambda = require("luasnip.extras").l
+-- local postfix = require("luasnip.extras.postfix").postfix
 
 luasnip.config.set_config {
   -- This tells LuaSnip to remember to keep around the last snippet.
@@ -6,7 +21,7 @@ luasnip.config.set_config {
   history = true,
 
   -- This one is cool cause if you have dynamic snippets, it updates as you type!
-  -- updateevents = "TextChanged,TextChangedI",
+  updateevents = "TextChanged,TextChangedI",
 
   -- Autosnippets:
   enable_autosnippets = true,
@@ -33,4 +48,60 @@ vim.keymap.set(
 vim.keymap.set(
   "i", "<c-l>",
     function() if luasnip.choice_active() then luasnip.change_choice(1) end end
+)
+
+-- The snippets
+
+luasnip.add_snippets(
+  "javascript", {
+    snip(
+      "import", fmt(
+        [[import {{ {} }} from "{}"]], { i(2, "name"), i(1, "package-name") },
+          { desc = "xxxxImport a named thing" }
+      )
+    ),
+
+    snip(
+      "import", fmt(
+        [[import {} from "{}"]], { i(1, "name"), i(2, "package-name") },
+          { desc = "Import a default thing" }
+      )
+    ),
+
+    snip(
+      "useState", fmt(
+        [[const [{}] = useState({})]], {
+          f(
+            function(args)
+              return args[1][1] .. ", " .. "set" ..
+                       args[1][1]:gsub("^%l", string.upper)
+            end, { 1 }
+          ),
+          i(1),
+        }
+      )
+    ),
+
+    snip(
+      "useEffect", fmt(
+        [[useEffect(() => {{
+  {}
+}}, [{}])
+]], { i(1), i(2) }
+      )
+    ),
+  }
+)
+
+luasnip.filetype_extend(
+  "javascript", { "javascriptreact", "typescript", "typescriptreact" }
+)
+luasnip.filetype_extend(
+  "javascriptreact", { "javascript", "typescript", "typescriptreact" }
+)
+luasnip.filetype_extend(
+  "typescript", { "javascriptreact", "javascript", "typescriptreact" }
+)
+luasnip.filetype_extend(
+  "typescriptreact", { "javascriptreact", "typescript", "javascript" }
 )
