b0231cc8080d68ffa8bdc35f720b5338e4d47215
Steve Occhipinti <dev@stevenocchipinti.com>
2022-07-21 22:14:52 +1000

    Use nice rounded bordered for neotree

--- plugin/neotree.lua
+++ plugin/neotree.lua
@@ -3,14 +3,9 @@ vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 
 vim.keymap.set("n", "<leader>f", ":Neotree reveal float\n", { silent = true })
 
--- LuaFormatter off
 require("neo-tree").setup {
   filesystem = { follow_current_file = true },
-  window = {
-    mappings = {
-      ["l"] = "open",
-      ["h"] = "close_node",
-    },
-  },
+  hijack_netrw_behavior = "open_current",
+  popup_border_style = "rounded",
+  window = { mappings = { ["l"] = "open", ["h"] = "close_node" } },
 }
--- LuaFormatter on
