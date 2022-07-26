6bad182986062e05a2e66b3cec227d983d60dbd3
Steve Occhipinti <dev@stevenocchipinti.com>
2022-07-26 21:54:13 +1000

    Let NeoTree handle opening directories
    This is useful when launching `vim .` or `:vs .` to browse a directory.

--- plugin/neotree.lua
+++ plugin/neotree.lua
@@ -4,8 +4,10 @@ vim.cmd [[ let g:neo_tree_remove_legacy_commands = 1 ]]
 vim.keymap.set("n", "<leader>f", ":Neotree reveal float\n", { silent = true })
 
 require("neo-tree").setup {
-  filesystem = { follow_current_file = true },
-  hijack_netrw_behavior = "open_current",
+  filesystem = {
+    follow_current_file = true,
+    hijack_netrw_behavior = "open_current",
+  },
   popup_border_style = "rounded",
   window = { mappings = { ["l"] = "open", ["h"] = "close_node" } },
 }
