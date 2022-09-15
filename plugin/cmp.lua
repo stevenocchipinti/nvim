75fa327b8d82af6fb1dc2a7f8ef8580785c276df
Steve Occhipinti <dev@stevenocchipinti.com>
2022-09-15 14:16:47 +1000

    Allow buffer autocomplete without the 5 char limit
    I realised I use buffer completion way more than I thought and I don't often
    type 5 chars before I need it :P

--- plugin/cmp.lua
+++ plugin/cmp.lua
@@ -27,7 +27,7 @@ cmp.setup(
         { name = "nvim_lsp" },
         { name = "luasnip" },
         { name = "path" },
-        { name = "buffer", keyword_length = 5 },
+        { name = "buffer" },
       }
     ),
   }
