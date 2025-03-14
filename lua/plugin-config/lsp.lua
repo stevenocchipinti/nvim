f8a169fdd8d12e85398231d57e2f62ba419bff5e
Steve Occhipinti <dev@stevenocchipinti.com>
2025-03-14 21:32:30 +1100

    wip

--- lua/plugin-config/lsp.lua
+++ lua/plugin-config/lsp.lua
@@ -89,7 +89,8 @@ lspconfig.lua_ls.setup {
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
-                globals = {"vim"}
+                globals = {"vim"},
+                disable = { "missing-fields" },
             },
             workspace = {
                 -- Make the server aware of Neovim runtime files
