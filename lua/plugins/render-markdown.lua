6cae3688b7bcfd8b63796bc04f09767acb169b0b
Steve Occhipinti <dev@stevenocchipinti.com>
2025-04-08 20:57:41 +1000

    Add some fanciness to markdown files
    Fanciness can be toggled off with <leader>um
    Although, the links part is done with "conceal" so that one is <leader>uc

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 3,
+      },
+      heading = {
+        sign = false,
+        icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
+        border = true,
+      },
+      checkbox = {
+        enabled = true,
+      },
+    },
+  },
+}
